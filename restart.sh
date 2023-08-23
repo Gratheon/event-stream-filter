40b118e15697d69cd89ff909e3bda81c87b33034
Artjom Kurapov <artjom.kurapov@clarifai.com>
2023-08-24 02:38:44 +0300

    cleanup

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
-sudo -H -u www bash -c 'cd /www/event-stream-filter/ && npm i && npm build' 
+sudo -H -u www bash -c 'cd /www/event-stream-filter/' 
 cd /www/event-stream-filter/
 COMPOSE_PROJECT_NAME=gratheon docker-compose down
 COMPOSE_PROJECT_NAME=gratheon docker-compose up -d --build
